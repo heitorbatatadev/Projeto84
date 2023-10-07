d6912a9afd80fef99993d58983c1cc99d40bb8cd
heitorbatatadev <129177617+heitorbatatadev@users.noreply.github.com>
2023-10-07 17:08:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+canvas = document.getElementById('myCanvas');
+ctx= canvas.getContext("2d");
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); 
+	img_imgTag.onload = uploadimg; 
+	img_imgTag.src = img_image;   
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+window.addEventListener("keydown" , my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		
+		aplhabetkey();
+		document.getElementById("d1").innerHTML="Você pressionou uma tecla alfabeto";
+		console.log("alphabet key");
+	}
+}
+
+function aplhabetkey()
+{
+	img_image="alfabeto.png";
+	add();
+}
+function numberkey()
+{
+	img_image="numero.png";
+	add():
+}
+function arrowkey()
+{
+	img_image="direcional.png";
+	add();
+}
+function specialkey()
+{
+	img_image="especial.png";
+	add();
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
